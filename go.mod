e4aed513c8097f9be85d20b2e6532299476585ce
barraotieno <barrackotieno160@gmail.com>
2024-07-17 17:34:32 +0300

    add main.go file to start a server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module web-pract
+
+go 1.22.2
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+// helloHandler() function handles http requests and responds with 'Hello Web ;)'
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+	// w.Header().Set("Content-type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "Hello Web ;)")
+}
+
 func main() {
+
+	//Register the helloHandler function as the handler for the "/" path
+	http.HandleFunc("/", helloHandler)
+
+	// Start th http server on port 8080
+	fmt.Println("Starting server on: 8080")
 	
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+
 }
